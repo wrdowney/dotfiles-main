4bbdb3a3cba925bc246daf05c1b60f230a08f309
wrdowney <wdowney20@gmail.com>
2024-08-06 16:17:43 -0400

    Initial commit

---
+++ home-manager/git.nix
@@ -0,0 +1,21 @@
+let
+  name = "wrdowney";
+in {
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      color.ui = true;
+      core.editor = "nvim";
+      credential.helper = "store";
+      github.user = name;
+      push.autoSetupRemote = true;
+    };
+    userEmail = "wdowney20@gmail.com";
+    userName = name;
+  };
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+  };
+  services.ssh-agent.enable = true;
+}
---
+++ home-manager/packages.nix
@@ -0,0 +1,34 @@
+{pkgs, ...}: {
+  imports = [
+    ./modules/packages.nix
+    ./scripts/blocks.nix
+    ./scripts/nx-switch.nix
+    ./scripts/vault.nix
+  ];
+
+  packages = with pkgs; {
+    linux = [
+      (mpv.override {scripts = [mpvScripts.mpris];})
+      spotify
+      # gnome-secrets
+      fragments
+      figma-linux
+      # yabridge
+      # yabridgectl
+      # wine-staging
+      nodejs
+      vscode
+      todoist-electron
+    ];
+    cli = [
+      bat
+      eza
+      fd
+      ripgrep
+      fzf
+      lazydocker
+      lazygit
+      htop
+    ];
+  };
+}
